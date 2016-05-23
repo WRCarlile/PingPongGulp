8f3e842797d0a86f5bd14136f5c452d8af46df7c
Ned Faulhaber and Ryan Carlile <ryan@>
2016-05-23 15:42:35 -0700

    Finish ping pong template

--- js/browser.js
+++
@@ -1,9 +0,0 @@
-var email = require('./email.js').email;
-
-$(document).ready(function(){
-  $('#email').submit(function(event){
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = email(goal);
-  });
-});
